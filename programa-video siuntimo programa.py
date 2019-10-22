af928333645bad0307cbd1da4e732682ed149582
daiva93 <55634673+daiva93@users.noreply.github.com>
2019-10-22 16:19:29 +0300

    Update programa-video siuntimo programa.py

--- programa-video siuntimo programa.py
+++ programa-video siuntimo programa.py
@@ -1,33 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
 
 
+import sys
+arguments = sys.argv[1:]
+filename = arguments[0]
 import youtube_dl 
+stream = yt.streams.first()
+stream.download('C:\\Users\')
 y= {} 
-while (1): 
+while True: 
     link= input("Copy & paste the URL of the YouTube video you want to download:-") 
     youtube_dl.YoutubeDL(y).download([link])
     f=input("\nENETR YES(Y) TO CONTINUE DOWNLOADING VIDEOS \nELSE NO(N) TO STOP\n") 
     if f=="N" or f=="n":
         break
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
